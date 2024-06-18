30a6334ffaff8fc7fdce2de03ada14da7e6d7856
rna3 <rnalcaraz3@gmail.com>
2024-06-18 11:22:37 -0700

    finished model

---
+++ caroo.js
@@ -0,0 +1,50 @@
+class Vehicle {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+    honk() {
+      return "Beep Beep";
+    }
+    toString() {
+      return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+  }
+  
+  class Car extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 4;
+    }
+  }
+  
+  class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+      super(make, model, year);
+      this.numWheels = 2;
+    }
+    revEngine() {
+      return "VROOOM!!!";
+    }
+  }
+  
+  class Garage {
+    constructor(capacity) {
+      this.vehicles = [];
+      this.capacity = capacity;
+    }
+    add(newVehicle) {
+      if (!(newVehicle instanceof Vehicle)) {
+        return "only vehicles are allowed";
+      }
+  
+      if (this.vehicles.length >= this.capacity) {
+        return "sorry we're full";
+      }
+  
+      this.vehicles.push(newVehicle);
+  
+      return "Vehicle Added!";
+    }
+  }
